fccde9d2fa82c1cd80372299ae997ffd7aa59c4e
ezeszuchet <szuchetezequiel@gmail.com>
2021-06-22 01:23:01 -0300

    Get and add products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,22 @@
+const Product = require('../models/product');
 
-exports.getProducts = (req, res, next) => {
+// Create new product => /api/v1/product/new
+exports.newProduct = async (req, res, next) => {
+    const product = await Product.create(req.body);
+    
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
+// Get all products => /api/v1/products
+exports.getProducts = async (req, res, next) => {
+
+    const products = await Product.find();
     res.status(200).json({
         success: true,
-        message: 'This route will show all products in database.'
+        count: products.length,
+        products
     })
 }
