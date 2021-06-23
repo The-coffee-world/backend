09bda06a28122558cdfde1588bb6768a784f6f8f
ezeszuchet <szuchetezequiel@gmail.com>
2021-06-23 08:53:57 -0300

    Backend error handling

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const connectDatabase = require('./config/database');
 
 const dotenv = require('dotenv');
 
+// Handle uncaught exceptions
+process.on('uncaughtException', err => {
+    console.log(`ERROR: ${err.stack}`);
+    console.log(`Shutting down server due to uncaught exception`);
+    process.exit(1)
+})
 
 // Setting up config file
 dotenv.config({ path: './config/config.env' })
@@ -12,6 +18,15 @@ dotenv.config({ path: './config/config.env' })
 // Connecting to Database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
+})
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', err => {
+    console.log(`ERROR: ${err.message}`);
+    console.log('Shutting down the server due to unhandled promise rejection');
+    server.close(() => {
+        process.exit(1);
+    })
 })
