1e604a2d38ea1ee965669af120a6597e2d665d3b
ezeszuchet <szuchetezequiel@gmail.com>
2021-07-06 09:40:49 -0300

    Update profile and passwords requests

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser, logout, forgotPassword, resetPassword } = require('../controllers/authController');
+const {
+    registerUser,
+    loginUser,
+    logout,
+    getUserProfile,
+    forgotPassword,
+    resetPassword,
+    updatePassword,
+    updateProfile
+} = require('../controllers/authController');
+
+const { isAuthenticatedUser } = require('../middlewares/auth')
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
@@ -10,5 +21,9 @@ router.route('/password/forgot').post(forgotPassword)
 router.route('/password/reset/:token').put(resetPassword)
 
 router.route('/logout').get(logout);
+
+router.route('/me').get(isAuthenticatedUser, getUserProfile);
+router.route('/password/update').put(isAuthenticatedUser, updatePassword);
+router.route('/me/update').put(isAuthenticatedUser, updateProfile);
 
 module.exports = router;
